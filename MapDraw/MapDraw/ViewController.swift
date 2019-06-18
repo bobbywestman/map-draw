dedd32d1fb257f0cad325f186828940b74f4febc
Bobby Westman <bobby.westman@hotmail.com>
2019-06-17 19:27:19 -0500

    project init

---
+++ MapDraw/MapDraw/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapDraw
+//
+//  Created by Bobby Westman on 6/17/19.
+//  Copyright © 2019 Bobby Westman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
